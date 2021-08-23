780427857c1dcb5096774d9d642f99802961370b
Rafael Matiello <rafael.matiello@gmail.com>
2021-08-23 20:23:47 -0300

    - iteração com o controle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
-import {BrowserModule} from '@angular/platform-browser';
+import {BrowserModule, HAMMER_GESTURE_CONFIG, HammerGestureConfig, HammerModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import { ScoreboardComponent } from './scoreboard/scoreboard.component';
+import {ScoreboardComponent} from './scoreboard/scoreboard.component';
 import {ButtonModule} from 'primeng/button';
+import * as Hammer from 'hammerjs';
+import {ConfirmDialogModule} from 'primeng/confirmdialog';
+
+
+export class MyHammerConfig extends HammerGestureConfig {
+  overrides = <any> {
+    swipe: {direction: Hammer.DIRECTION_ALL},
+  };
+}
 
 @NgModule({
   declarations: [
@@ -16,9 +25,14 @@ import {ButtonModule} from 'primeng/button';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    ButtonModule
+    ButtonModule,
+    HammerModule,
+    ConfirmDialogModule
   ],
-  providers: [],
+  providers: [{
+    provide: HAMMER_GESTURE_CONFIG,
+    useClass: MyHammerConfig,
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule {
--- src/app/scoreboard/scoreboard.component.ts
+++ src/app/scoreboard/scoreboard.component.ts
@@ -1,17 +1,24 @@
 import {Component, OnInit} from '@angular/core';
 import {ScoreboardService} from './scoreboard.service';
 import {ScoreboardDTO} from './scoreboard-dto';
+import {ConfirmationService, ConfirmEventType, MessageService} from 'primeng/api';
 
 @Component({
   selector: 'app-scoreboard',
   templateUrl: './scoreboard.component.html',
-  styleUrls: ['./scoreboard.component.scss']
+  styleUrls: ['./scoreboard.component.scss'],
+  providers: [ConfirmationService, MessageService]
 })
 export class ScoreboardComponent implements OnInit {
 
   scoreboard = new ScoreboardDTO();
+  public eventText: string;
+  private position: string;
+  private confirmation: ConfirmationService;
 
-  constructor(private service: ScoreboardService) {
+  constructor(private service: ScoreboardService,
+              private confirmationService: ConfirmationService,
+              private messageService: MessageService) {
   }
 
   ngOnInit(): void {
@@ -29,12 +36,60 @@ export class ScoreboardComponent implements OnInit {
   }
 
   reset(): void {
-    this.service.reset();
-    this.scoreboard = this.service.getScoreboard();
+    this.confirmPosition('bottom');
   }
 
   back(): void {
     this.service.back();
     this.scoreboard = this.service.getScoreboard();
+  }
+
+
+  onSwipe(evt): void {
+    const x = Math.abs(evt.deltaX) > 40 ? (evt.deltaX < 0 ? 'right' : 'left') : '';
+    const y = Math.abs(evt.deltaY) > 40 ? (evt.deltaY < 0 ? 'down' : 'up') : '';
+    if (y === 'up') {
+      this.sum('A');
+    }
+
+    if (y === 'down') {
+      this.sum('B');
+    }
+
+    if (x === 'left') {
+      this.back();
+    }
+
+    if (x === 'right') {
+      this.reset();
+    }
+
+    this.eventText = `${x} ${y}/ ${evt.deltaX} - ${evt.deltaY}`;
+  }
+
+
+  confirmPosition(position: string): void {
+    this.position = position;
+
+    this.confirmation = this.confirmationService.confirm({
+      message: 'Do you want to delete this record?',
+      header: 'Delete Confirmation',
+      icon: 'pi pi-info-circle',
+      key: 'positionDialog'
+    });
+  }
+
+  onSwipeConfirmation(evt: any): void {
+    const x = Math.abs(evt.deltaX) > 40 ? (evt.deltaX < 0 ? 'right' : 'left') : '';
+
+    if (x === 'left') {
+      this.confirmation.close();
+    }
+
+    if (x === 'right') {
+      this.confirmation.close();
+      this.service.reset();
+      this.scoreboard = this.service.getScoreboard();
+    }
   }
 }
